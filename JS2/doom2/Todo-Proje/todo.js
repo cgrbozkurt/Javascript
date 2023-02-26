610e2d30f4a1847ce2d843e6d65aac960f3087c7
cgrbozkurt <120256225+cgrbozkurt@users.noreply.github.com>
2023-02-26 23:40:57 +0300

    proje

--- JS2/doom2/Todo-Proje/todo.js
+++ JS2/doom2/Todo-Proje/todo.js
@@ -3,19 +3,23 @@ const todoInput=document.querySelector("#todo");
 const todoList=document.querySelector(".list-group");
 const firstCardBody=document.querySelectorAll(".card-body")[0];
 const secondCardBody=document.querySelectorAll(".card-body")[1];
-const filter=document.querySelector("#filter")
+const filter=document.querySelector("#filter");
 const clearButton=document.querySelector("#clear-todos");
 
 
-// evenListeners();
+eventListener();
 
-
-function addEventListener(){
-    form.evenListeners("submit",addTodo);
+function eventListener(){
+    form.addEventListener("submit",addTodo);
 
 }
 
 function addTodo(e){
-    const newTodo=todoInput.value;
+    const newTodo=todoInput.value.trim();
     console.log(newTodo);
+
+
+
+
+e.preventDefault();
 }
