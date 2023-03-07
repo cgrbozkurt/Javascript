32b764f2de9e386e67f6eec711f5bc4a1a462a40
cgrbozkurt <cagribozzkurt@gmail.com>
2023-03-07 13:10:03 +0300

    günlük 4

---
+++ zdenemeler/d.js
@@ -0,0 +1,22 @@
+const todoForm=document.getElementById("todo-form");
+// const filter=document.getElementById("filter");
+// const button=document.getElementsByClassName("btn");
+
+todoForm.addEventListener("submit" , submitForm);
+
+function submitForm(e){
+    console.log("naber");
+
+    e.preventDefault();
+}
+ 
+
+// function submitForm(e){
+//     console.log("naber");
+
+//     e.preventDefault();
+// }
+
+
+
+
