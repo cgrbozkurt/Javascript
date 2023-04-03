f8dafca5e513eab1d0667ac7043192d5c234dbe7
cgrbozkurt <cagribozzkurt@gmail.com>
2023-04-04 02:59:44 +0300

    loto

--- zTabindex/tabindex.js
+++ zTabindex/tabindex.js
@@ -4,26 +4,62 @@ let x3=document.getElementById("green-box");
 let x4=document.getElementById("yellow-box");
 let x5=document.getElementById("black-box");
 
+let button1=document.getElementById("button1");
+
+
+function addEvent(){
+    button1.addEventListener("click",showRed());
+    
+}
+
+
+
+// let x=[x1,x2,x3,x4,x5];
+
+// function showRed(){
+//     x1.style.display="block";
+// }
+// function showBlue(){
+//     x2.style.display="block";
+// }
+
+
+
+
+
 function showRed(){
-
-
+    divKapat();
+x1.style.display="block"
 
 }
 
 function showBlue(){
-    
+    divKapat();
+    x2.style.display="block";
     
 }
 
 function showGreen(){
-    
+    divKapat();
+x3.style.display="block"
 }
 
 function showYellow(){
-   
-   
+    divKapat();
+    x4.style.display="block"
+
 }
 function showBlack(){
-    
+    divKapat();
+    x5.style.display="block"
+
    
+}
+
+function divKapat(){
+    x1.style.display="none";
+    x2.style.display="none";
+    x3.style.display="none";
+    x4.style.display="none";
+    x5.style.display="none";
 }
