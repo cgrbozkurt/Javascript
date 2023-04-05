4fa1f2a0289580e7fc886fb6729a067685c2b80c
cgrbozkurt <cagribozzkurt@gmail.com>
2023-04-05 23:58:09 +0300

    destruction

--- ES6+---1/arrow.js
+++ ES6+---1/arrow.js
@@ -8,3 +8,17 @@ const cube = x => {
     return x*x*x;
 }
 document.body.innerHTML=cube(4);
+
+//DESTRUCTİNG
+
+let number1,number2;
+arr=[1,2,3,4];
+[number1,number2]=arr;
+// [number1,number2]=[1,2,3,4];=arr; olduğu için number 1 bire number2 ikiye eşit oluyor 
+
+const cars={  a:10,b:20,c:30,d:40,e:50 };
+
+const {a:numbera,b:numberb,e:numberc}=cars;
+
+// console.log(a,b,e);
+console.log(numbera,numberb,numberc);
