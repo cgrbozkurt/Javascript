88250a9bc31da4dc55e8c6b0610dc1bdc38edc45
cgrbozkurt <cagribozzkurt@gmail.com>
2023-04-09 23:47:41 +0300

    referans

--- ES6+---1/forin-of-Map.js
+++ ES6+---1/forin-of-Map.js
@@ -52,3 +52,56 @@ myMap.set(key3,"Function Değeri");
 
 console.log(myMap.size);
 console.log(myMap);
+
+
+//FOR EACH DÖNGÜSÜYLE 
+const forMap=new Map();
+
+forMap.set("kayseri",120);
+forMap.set("ankara",1321);
+forMap.set("adana",1231);
+forMap.set("yozgat","sorgun","trabzon"); /* 1. değer key 2. değer value 3 ve daha sonraki değerleri okumaz*/
+// console.log(forMap);
+forMap.forEach(function(key,value){
+    console.log(value,key);
+});
+
+
+
+
+//For of dönüsü ile gezinmek
+
+for(let [key,value] of myMap){
+    document.body.innerHTML={key,value};
+}
+let a="baybars";
+let b="baybars";
+
+if(a===b){
+    console.log("Eşit");}
+
+
+    let array1=[1,2,3,4,5];
+    let array2=[1,2,3,4,5];
+    if(array1===array2){
+        console.log("Eşit");
+    
+    }
+
+    const person1={
+    name:"Baybars",
+    age:25
+    }
+    const person2={
+        name:"Baybars",
+        age:25
+    }
+    if(person1===person2){
+        console.log("eşit");
+    }
+
+    const cities=new Map();
+    const key=[1,2,3];
+    cities.set("Ankara",5);
+
+
