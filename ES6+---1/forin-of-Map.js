c2fb7143cdbc88827580c36474a50da32f92713f
cgrbozkurt <cagribozzkurt@gmail.com>
2023-04-16 17:57:52 +0300

    re

--- ES6+---1/forin-of-Map.js
+++ ES6+---1/forin-of-Map.js
@@ -1,7 +1,10 @@
 const person={
-    name:"Cagri",
+    name:":"+" "+" Cagri",
     age:30,
-    salary:3000+"usd"
+    salary:3000+"usd",
+    gender:"Male",
+    job:"null"
+
 };
 
 const langs=["html","java","c#"];
@@ -14,13 +17,13 @@ for(let prop in person){
 console.log(prop,person[prop]);
 }
 //ARRAY
-for(let index in langs){
-    console.log(index,langs[index]);
-}
-//STRİNG
-for(let index in name){
-    console.log(index,name[index]);
-}
+// for(let index in langs){
+//     console.log(index,langs[index]);
+// }
+// //STRİNG
+// for(let index in name){
+//     console.log(index,name[index]);
+// }
 
 
 //OBJECT let value of objectle kullanılmaz
@@ -40,68 +43,69 @@ for(let index in name){
 
 // / MAP 
 
-const myMap=new Map();
+// const myMap=new Map();
 
-const key1="Baybars";
-const key2={a:10,b:20};
-const key3=myFunc=()=> 2;
+// const key1="Baybars";
+// const key2={a:10,b:20};
+// const key3=myFunc=()=> 2;
 
-myMap.set(key1,"String Değeri");
-myMap.set(key2,"Object Değeri");
-myMap.set(key3,"Function Değeri");
+// myMap.set(key1,"String Değeri");
+// myMap.set(key2,"Object Değeri");
+// myMap.set(key3,"Function Değeri");
 
-console.log(myMap.size);
-console.log(myMap);
-
-
-//FOR EACH DÖNGÜSÜYLE 
-const forMap=new Map();
-
-forMap.set("kayseri",120);
-forMap.set("ankara",1321);
-forMap.set("adana",1231);
-forMap.set("yozgat","sorgun","trabzon"); /* 1. değer key 2. değer value 3 ve daha sonraki değerleri okumaz*/
-// console.log(forMap);
-forMap.forEach(function(key,value){
-    console.log(value,key);
-});
+// console.log(myMap.size);
+// console.log(myMap);
 
 
+// //FOR EACH DÖNGÜSÜYLE 
+// const forMap=new Map();
+
+// forMap.set("kayseri",120);
+// forMap.set("ankara",1321);
+// forMap.set("adana",1231);
+// forMap.set("yozgat","sorgun","trabzon"); /* 1. değer key 2. değer value 3 ve daha sonraki değerleri okumaz*/
+// // console.log(forMap);
+// forMap.forEach(function(key,value){
+//     console.log(value,key);
+// });
 
 
-//For of dönüsü ile gezinmek
-
-for(let [key,value] of myMap){
-    document.body.innerHTML={key,value};
-}
-let a="baybars";
-let b="baybars";
-
-if(a===b){
-    console.log("Eşit");}
 
 
-    let array1=[1,2,3,4,5];
-    let array2=[1,2,3,4,5];
-    if(array1===array2){
-        console.log("Eşit");
+// //For of dönüsü ile gezinmek
+
+// for(let [key,value] of myMap){
+//     document.body.innerHTML={key,value};
+// }
+// let a="baybars";
+// let b="baybars";
+
+// if(a===b){
+//     console.log("Eşit");}
+
+
+//     let array1=[1,2,3,4,5];
+//     let array2=[1,2,3,4,5];
+//     if(array1===array2){
     
-    }
+//         console.log("Eşit");
+    
+//     }
 
-    const person1={
-    name:"Baybars",
-    age:25
-    }
-    const person2={
-        name:"Baybars",
-        age:25
-    }
-    if(person1===person2){
-        console.log("eşit");
-    }
+//     const person1={
+//     name:"Baybars",
+//     age:25
+//     }
+//     const person2={
+//         name:"Baybars",
+//         age:25
+//     }
+//     if(person1===person2){
+//         console.log("eşit");
+//     }
 
-    const cities=new Map();
-    const key=[1,2,3];
-    cities.set("Ankara",5);
+//     const cities=new Map();
+//     const key=[1,2,3];
+//     cities.set("Ankara",5);
 
 
