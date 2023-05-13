f05a20c3ca474d6df6e16ce24983d0a31a3b3064
cgrbozkurt <cagribozzkurt@gmail.com>
2023-05-13 16:23:17 +0300

    tkm

--- TAŞ KAĞIT MAKAS/tkm.js
+++ TAŞ KAĞIT MAKAS/tkm.js
@@ -1,34 +1,147 @@
-const gamerSkor=document.querySelector("#gamer-skor");
-const pcSkor=document.getElementsByClassName("pc-skor");
+const gamerSkor=document.getElementById("gamer-skor")
+const pcSkor=document.getElementById("pc-skor");
 const tas=document.getElementById("tasButton");
-const kagit=document.getElementsByClassName("kagit");
-const makas=document.getElementsByClassName("makas");
+const kagit=document.getElementById("kagitButton");
+const makas=document.getElementById("makasButton");
 const pcBey=document.getElementsByClassName("pc-bey");
 const gamerChoose=document.getElementById("oyuncu-secim");
-const savas=document.getElementsByClassName("savas");
+const savas=document.getElementById("savas");
 const PcChoose=document.getElementById("pc-sec");
 const deneme=document.getElementById("secilen");
+let a="";
 
-
-let oyuncu=0;
-let bilgsisayar=0;
+let oyuncu=null;
+let bilgisayar=null;
 
 eventListeneer();
 
-function bilgisayarSecim(){
-    const secim=["tas","kagit","makas"];
+
+
+function bilgisayarSecim(e){
+    const secim=["Taş","Makas","Kağıt"];
     const rastgeleSecim=Math.floor(Math.random()*3);
-    PcChoose.innerHTML=secim[rastgeleSecim];
+    PcChoose.innerHTML= secim[rastgeleSecim];
+    // setTimeout(() => {
+    //     PcChoose.innerHTML="?"
+    // }, 2500);
     return secim[rastgeleSecim];
+
+
 }
 
 function eventListeneer(){
-tas.addEventListener("click",tasSecim)
+tas.addEventListener("click",function(){
+    deneme.innerHTML=tas.innerHTML
+    // setTimeout(() => {
+    //     deneme.innerHTML="?"
+    // }, 2500);
+    bilgisayarSecim();
+    let bilSecim=bilgisayarSecim();
+    sonuc("Taş",bilSecim);
+   
+
+});
+
+kagit.addEventListener("click",function(){
+    deneme.innerHTML=kagit.innerHTML
+    // setTimeout(() => {
+    //     deneme.innerHTML="?"
+    // }, 5500);
+    let bilSecim=bilgisayarSecim();
+   
+
+    bilgisayarSecim();
+    sonuc("Kağıt",bilSecim);
+    
+});
+makas.addEventListener("click",function(){
+    deneme.innerHTML=makas.innerHTML
+    // setTimeout(() => {
+    //     deneme.innerHTML="?"
+    // }, 2500);
+    let bilSecim=bilgisayarSecim();
+    
+
+    bilgisayarSecim();
+    sonuc("Makas",bilSecim);
+}
+);
+}
+
+function sonuc(){
+
 }
     
 
-function tasSecim(){
-deneme.innerHTML="taş"
-    console.log("taş");
+function sonuc(oyuncu,bilgisayar){
+    switch(oyuncu+bilgisayar){
+
+        case "TaşMakas":
+            case "MakasKağıt":
+            case "KağıtTaş":
+                kazan();
+            break;
+
+            case "TaşKağıt":
+            case "KağıtMakas":
+            case "MakasTaş":
+                kaybet();
+                break;
+
+                case "TaşTaş":
+                case "KağıtKağıt":
+                case "MakasMakas":
+                    berabere();
+                    break;
+                    
+                    default : "Hata";
+
+
+
+    }
+
 }
+
+
+
+function kazan(){
+    savas.innerHTML="KAZANDINIZ";
+oyuncu=+gamerSkor.innerHTML;
+oyuncu++;
+gamerSkor.innerHTML=oyuncu;
+
+// setTimeout(() => {
+//     savas.innerHTML=""
+//    }, 5500);
+console.log("kazqan");
+
+}
+
+function kaybet(){
+//    bilgisayar=bilgisayar+1;
+//    pcSkor.innerHTML=bilgisayar.toString();
+   savas.innerHTML="KAYBETTİN DÜDÜK";
+   bilgisayar=+pcSkor.innerHTML;
+   bilgisayar++;
+   pcSkor.innerHTML=bilgisayar;
+   console.log("kaybet");
+//    setTimeout(() => {
+//     savas.innerHTML=""
+//    }, 5500);
+}
+ 
+// gamerSkor.innerHTML=0123;
+function berabere(){
+    savas.innerHTML="BERABERE";
+    // setTimeout(() => {
+    //     savas.innerHTML=""
+    //    }, 5500);
+    console.log("berabere");
+}
+
+
+
+
+
+
 
