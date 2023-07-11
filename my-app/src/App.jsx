d9e68939211b9f482148f1d4efa844af01e7ef27
cgrbozkurt <cagribozzkurt@gmail.com>
2023-07-11 10:24:26 +0300

    React2.gun

---
+++ my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import ProductItem from './Components/ProductItems';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PRODUCTS</h1>
+    <ProductItem></ProductItem>
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/ProductItems.jsx
@@ -0,0 +1,13 @@
+function ProductItem() {
+  return(
+    <div>
+        <div>Product İmage</div>
+        <h2>Product Tittle</h2>
+        <span>Product Price</span>
+    </div>
+  );
+}
+
+
+
+export default ProductItem;
