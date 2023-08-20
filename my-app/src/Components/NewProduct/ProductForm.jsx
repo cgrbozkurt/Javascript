b5c063d8b09061c77481971d664c9df16fd36035
cgrbozkurt <cagribozzkurt@gmail.com>
2023-08-20 23:38:52 +0300

    react

--- my-app/src/Components/NewProduct/ProductForm.jsx
+++ my-app/src/Components/NewProduct/ProductForm.jsx
@@ -1,24 +1,61 @@
+import { useState } from "react";
 import "./ProductForm.css";
 
 const ProductForm = () => {
-    const titleChangeHandler=(e)=>{
-        console.log(e.target.value);
-    }
+  // const [ productName,setProductName]=useState("");
+  // const [ productPrice,setProductPrice]=useState("");
+  // const [ productUrl,setProductUrl]=useState("");
+
+  const [productData,setProductData]=useState({
+    productName:"",
+    productPrice:"",
+    productPrice:"",
+
+  });
+
+  // const productChangeHandler=()=>{
+  //   setProductData()
+  // }
+
+  const titleChangeHandler = (e) => {
+    setProductData({
+      ...productData,
+      productName:e.target.value
+    });
+  };
+  const priceChangeHandler = (e) => {
+    setProductData({
+      ...productData,
+      productPrice:e.target.value
+    })
+  };
+  const urlChangeHandler = (e) => {
+    setProductData({
+      ...productData,
+      productUrl:e.target.value
+    })
+  };
   return (
     <form className="product-form">
       <div className="product-form-input">
-        <label htmlFor="">Ürün Adı</label>
-        <input type="text" name="" id="" placeholder="Ürün Giriniz" onChange={titleChangeHandler}/>
+        <label htmlFor="">{productName}</label>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Ürün Giriniz"
+          onChange={titleChangeHandler}
+        />
       </div>
       <div className="product-form-input">
-        <label htmlFor="">Ürün Fiyatı</label>
-        <input type="number" name="" id="" placeholder="Ürün Giriniz" />
+        <label htmlFor="">{productPrice}</label>
+        <input type="number" name="" id="" placeholder="Adet giriniz" onChange={priceChangeHandler}/>
       </div>
       <div className="product-form-input">
-        <label htmlFor="">Ürün Görseli</label>
-        <input type="text" name="" id="" placeholder="Ürün Giriniz" />
+        <label htmlFor=""></label>
+        <input type="text" name="" id="" placeholder="Resim Yükleyin" onChange={urlChangeHandler} />{productUrl}
       </div>
-      <button className="product-form-buttons">Ürün Ekle</button>
+      <button className="product-form-buttons">Ara</button>
     </form>
   );
 };
