9e662c5931b5a8aa2aae45f9371f9c2bc6bd384c
cgrbozkurt <cagribozzkurt@gmail.com>
2023-09-04 23:09:04 +0300

    react

--- react-custom-hooks/src/components/BackwardCounter.jsx
+++ react-custom-hooks/src/components/BackwardCounter.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 
 import Card from './Card';
+import useCounter from '../hooks/use-counter';
 
 const BackwardCounter = () => {
   const [counter, setCounter] = useState(0);
