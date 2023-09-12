fd692c0d4665a63f830685f57477d80734380912
cgrbozkurt <cagribozzkurt@gmail.com>
2023-09-12 21:03:03 +0300

    context

--- project1/src/components/CalculatorUseReducer.jsx
+++ project1/src/components/CalculatorUseReducer.jsx
@@ -1,22 +1,8 @@
 import { useReducer } from "react";
 import "./Calculator.css";
+import { initialState, reducer } from "./Reducer/reducerCalculator";
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "SET_NUMBER1":
-      return { ...state, number1: action.payload };
-    case "SET_NUMBER2":
-      return { ...state, number1: action.payload };
-    default:
-      throw new Error("invalid type action!");
-  }
-}
 
-const initialState = {
-  number1: 0,
-  number2: 0,
-  result: 0,
-};
 
 const CalculatorUseReducer = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -28,6 +14,11 @@ const CalculatorUseReducer = () => {
   };
   const handleNumber2Change = (e) => {
     dispatch({ type: "SET_NUMBER2", payload: Number(e.target.value) });
+  };
+
+  const handleAddition = () => {
+    dispatch({ type: "SET_RESULT", payload: state.number1 + state.number2 });
+    // setResult(number1 + number2);
   };
 
   return (
@@ -43,21 +34,35 @@ const CalculatorUseReducer = () => {
             type="number"
             id="number1"
             onChange={handleNumber1Change}
+            value={state.number1}
           />
         </div>
         <div className="form-group">
           <label className="form-label" htmlFor="number2">
             Number2
           </label>
-          <input className="form-input" type="number" id="number2"
-          onChange={handleNumber2Change} />
+          <input
+            className="form-input"
+            type="number"
+            id="number2"
+            onChange={handleNumber2Change}
+            value={state.number2}
+          />
         </div>
-        <button className="form-button">Add</button>
+        <button className="form-button" onClick={handleAddition}>
+          Add
+        </button>
         <div className="form-group">
           <label className="form-label" htmlFor="result">
             Result
           </label>
-          <input className="form-input" type="number" id="result" />
+          <input
+            className="form-input"
+            type="number"
+            id="result"
+            value={state.result}
+            readOnly
+          />
         </div>
       </div>
     </div>
